91424e414a07cb2f431b6be6f8386384cfe4a63f
Ashutosh Varma <ashutoshvarma11@live.com>
2023-02-20 17:09:51 +0530

    feat: use default serialization for tuple

--- packages/common/README.md
+++ packages/common/README.md
@@ -1,7 +1,38 @@
-# Ask! PSP34 Implementation
+# Ask! Tuple & Enum Support (Experimental)
 
-Implementation of PSP34 (NFTs) in Ask! targeting [Substrate](https://github.com/paritytech/substrate) FRAME [pallet-contracts](https://github.com/paritytech/substrate/tree/master/frame/contracts) with some limitations.
-Closely follows PSP34 but with some variations to compensate for limitations of Ask! & AssemblyScript.
+AssemblyScript does not supports tuples and tagged-union types (Rust-like enums) and so does ask!.
+The `Option<T>` and `Result<T, E>` are used extensively inside PSPs along with other enum types.
+Therefore having their support in ask! is really important before ask! can be used to onboard new
+developers from non Rust background.
 
-WARNING: This is an proof-of-concept prototype, and in particular has not received careful code review and tests.
-This implementation is NOT ready for production use.
+## Notes :-
+
+- Enums doesn't support spread layout storage. To add support for that we need to implement concept
+  similar to ink!v3's `FOOTPRINT` in spread layout to generate right keys.
+- All the types in storage should've no required argument in constructor, see https://github.com/ask-lang/ask/issues/245#issuecomment-1408074629
+- Tuple/Object with string field panics if included in contract storage.
+
+## Tuples
+
+Tuples in ask! are objects anointed with `@tuple()` decorator. All it does is instruct the metadata generator
+to output a Tuple type instead of a Composite one.
+
+Below type is equivalent to Rust's `(u64, bool)`
+
+```ts
+@tuple()
+class MyTuple {
+  constructor(public val0: u64 = 0, public val1: bool = false) {}
+}
+```
+
+Generic implementations for tuples are provided in ['tuples.ts'](./assembly/tuple.ts) and recommended over creating a new tuple type.
+
+## Enums
+
+Rust like enums (or tagged union types) can be defined using the combination of `@enumerator()` and `@variant()` decorators.
+As of now the serialization and storage layout are not supported by transform and are required to implement them manually.
+
+```
+
+```
--- packages/common/assembly/tuple.ts
+++ packages/common/assembly/tuple.ts
@@ -17,29 +17,33 @@ export class Empty {
 }
 
 // Tuple 1
+@serialize({ omitName: true })
+@deserialize({ omitName: true })
 @packedLayout()
 @spreadLayout()
 @tuple()
 export class Tuple1<T> {
   constructor(public val0: T = instantiateZero<T>()) {}
 
-  // @ts-ignore
-  serialize<__R, __S extends Serializer<__R>>(serializer: __S): __R {
-    serializer.startSerializeTuple();
-    serializer.serializeTupleElem(this.val0);
-    return serializer.endSerializeTuple();
-  }
+  // // @ts-ignore
+  // serialize<__R, __S extends Serializer<__R>>(serializer: __S): __R {
+  //   serializer.startSerializeTuple();
+  //   serializer.serializeTupleElem(this.val0);
+  //   return serializer.endSerializeTuple();
+  // }
 
-  // @ts-ignore
-  deserialize<__S extends Deserializer>(deserializer: __S): this {
-    deserializer.startDeserializeTuple(1);
-    this.val0 = deserializer.deserializeTupleElem<T>();
-    deserializer.endDeserializeTuple();
-    return this;
-  }
+  // // @ts-ignore
+  // deserialize<__S extends Deserializer>(deserializer: __S): this {
+  //   deserializer.startDeserializeTuple(1);
+  //   this.val0 = deserializer.deserializeTupleElem<T>();
+  //   deserializer.endDeserializeTuple();
+  //   return this;
+  // }
 }
 
 // Tuple 2
+@serialize({ omitName: true })
+@deserialize({ omitName: true })
 @packedLayout()
 @spreadLayout()
 @tuple()
@@ -49,25 +53,27 @@ export class Tuple2<T, V> {
     public val1: V = instantiateZero<V>(),
   ) {}
 
-  // @ts-ignore
-  serialize<__R, __S extends Serializer<__R>>(serializer: __S): __R {
-    serializer.startSerializeTuple();
-    serializer.serializeTupleElem(this.val0);
-    serializer.serializeTupleElem(this.val1);
-    return serializer.endSerializeTuple();
-  }
+  // // @ts-ignore
+  // serialize<__R, __S extends Serializer<__R>>(serializer: __S): __R {
+  //   serializer.startSerializeTuple();
+  //   serializer.serializeTupleElem(this.val0);
+  //   serializer.serializeTupleElem(this.val1);
+  //   return serializer.endSerializeTuple();
+  // }
 
-  // @ts-ignore
-  deserialize<__S extends Deserializer>(deserializer: __S): this {
-    deserializer.startDeserializeTuple(2);
-    this.val0 = deserializer.deserializeTupleElem<T>();
-    this.val1 = deserializer.deserializeTupleElem<V>();
-    deserializer.endDeserializeTuple();
-    return this;
-  }
+  // // @ts-ignore
+  // deserialize<__S extends Deserializer>(deserializer: __S): this {
+  //   deserializer.startDeserializeTuple(2);
+  //   this.val0 = deserializer.deserializeTupleElem<T>();
+  //   this.val1 = deserializer.deserializeTupleElem<V>();
+  //   deserializer.endDeserializeTuple();
+  //   return this;
+  // }
 }
 
 // Tuple 3
+@serialize({ omitName: true })
+@deserialize({ omitName: true })
 @packedLayout()
 @spreadLayout()
 @tuple()
@@ -78,22 +84,22 @@ export class Tuple3<T, V, U> {
     public val2: U = instantiateZero<U>(),
   ) {}
 
-  // @ts-ignore
-  serialize<__R, __S extends Serializer<__R>>(serializer: __S): __R {
-    serializer.startSerializeTuple();
-    serializer.serializeTupleElem(this.val0);
-    serializer.serializeTupleElem(this.val1);
-    serializer.serializeTupleElem(this.val2);
-    return serializer.endSerializeTuple();
-  }
+  // // @ts-ignore
+  // serialize<__R, __S extends Serializer<__R>>(serializer: __S): __R {
+  //   serializer.startSerializeTuple();
+  //   serializer.serializeTupleElem(this.val0);
+  //   serializer.serializeTupleElem(this.val1);
+  //   serializer.serializeTupleElem(this.val2);
+  //   return serializer.endSerializeTuple();
+  // }
 
-  // @ts-ignore
-  deserialize<__S extends Deserializer>(deserializer: __S): this {
-    deserializer.startDeserializeTuple(2);
-    this.val0 = deserializer.deserializeTupleElem<T>();
-    this.val1 = deserializer.deserializeTupleElem<V>();
-    this.val2 = deserializer.deserializeTupleElem<U>();
-    deserializer.endDeserializeTuple();
-    return this;
-  }
+  // // @ts-ignore
+  // deserialize<__S extends Deserializer>(deserializer: __S): this {
+  //   deserializer.startDeserializeTuple(2);
+  //   this.val0 = deserializer.deserializeTupleElem<T>();
+  //   this.val1 = deserializer.deserializeTupleElem<V>();
+  //   this.val2 = deserializer.deserializeTupleElem<U>();
+  //   deserializer.endDeserializeTuple();
+  //   return this;
+  // }
 }
